360bfa02c12abe0b000cb1aaa432878435873ca3
MANUCISNE <117774887+MANUCISNE@users.noreply.github.com>
2023-09-26 18:05:46 -0300

    change functions on Step4.test

--- __tests__/Step4.test.tsx
+++ __tests__/Step4.test.tsx
@@ -1,19 +1,43 @@
 import React from "react";
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Step4 from "@/src/pages/step4";
 
-describe('should render all content', () => {
-    const { getByText } = render(<Step4 />);
+jest.mock("../src/contexts/FormStepContext", () => ({
+  useGlobalContext: jest.fn(() => ({
+    setAdditional: jest.fn(),
+    setPlansMonth: jest.fn(),
+    setPlansYearly: jest.fn(),
+  })),
+}));
 
-    expect(getByText('Finishing up')).toBeInTheDocument();
+describe("Step3 component", () => {
+  it("should render the three add-ons", () => {
+    render(<Step4 />);
 
-    // Verifique se o preço do plano é renderizado
-    expect(getByText('$soma')).toBeInTheDocument();
-  
+    expect(screen.getByText('$soma')).toBeInTheDocument();
+
     // Verifique se o total do plano é renderizado
-    expect(getByText('${soma}/{typePlan ? "yr" : "mo"}')).toBeInTheDocument();
-  
+    expect(screen.getByText('${soma}/{typePlan ? "yr" : "mo"}')).toBeInTheDocument();
+
     // Verifique se o botão `Confirm` é renderizado
-    expect(getByText('Confirm')).toBeInTheDocument();
+    expect(screen.getByText('Confirm')).toBeInTheDocument();
   });
+
+  it('should render the correct data', () => {
+    // Simule os dados que serão passados para o componente
+    const data = {
+      title: 'My Plan',
+      plan: '(Month)',
+      price: 100,
+    };
+  
+    render(<Step4 data={data} />);
+  
+    // Verifique se o título do plano é renderizado corretamente
+    expect(screen.getByText('My Plan (Month)')).toBeInTheDocument();
+  
+    // Verifique se o preço do plano é renderizado corretamente
+    expect(screen.getByText('$100')).toBeInTheDocument();
+  });
+});
